58aa2aabe235665b7d0c0933a04abc7710a995f3
Bitaru <i@bitaru.com>
2023-06-14 14:35:20 +0200

    chore: make modules eager

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * as types from './types'
-export const components = import.meta.glob('./components/**/*.component.ts')
-export const buttons = import.meta.glob('./components/buttons/*.ts')
+export const components = import.meta.glob('./components/**/*.component.ts', { eager: true })
+export const buttons = import.meta.glob('./components/buttons/*.ts', { eager: true })
