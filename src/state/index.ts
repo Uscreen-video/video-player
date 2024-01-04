4738f71e1100d02e7393109547a0c7304d1fc2f2
Bitaru <i@bitaru.com>
2024-01-04 15:35:55 +0100

    fix(): chromecast icon

--- src/state/index.ts
+++ src/state/index.ts
@@ -17,7 +17,7 @@ export const initialState: State = {
   cues: [],
   castActivated: false,
   played: false,
-  castAvailable: Boolean(window.chrome?.cast),
+  castAvailable: Boolean(window.chrome?.cast?.isAvailable),
   airplayAvailable: Boolean((window as any).WebKitPlaybackTargetAvailabilityEvent),
   pipAvailable: Boolean(document.pictureInPictureEnabled),
   ...device
