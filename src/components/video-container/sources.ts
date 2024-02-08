baf79fbc669cfe95e07b260bfefb6f1688933a8b
timaramazanov <tima96ramazanov@gmail.com>
2024-02-08 12:39:46 +0000

    feat: added ability to lazy load player

---
+++ src/components/video-container/sources.ts
@@ -0,0 +1,24 @@
+import { State } from '../../types'
+
+export const sourcesController = (video: HTMLVideoElement) => {
+  const sources = Array.from(video.querySelectorAll('source'))
+
+  const supportedSource = sources.find(s => video.canPlayType(s.type))
+
+  const activeSource = supportedSource || sources[0]
+
+  return {
+    allSources: (): State['sources'] => sources.map(s => ({
+      type: s.type,
+      src: s.src || s.getAttribute('data-src')
+    })),
+    isSourceSupported: () => !!supportedSource,
+    getSrc: () => activeSource.src || activeSource.getAttribute('data-src'),
+    enableSource: () => {
+      activeSource.src = activeSource.src || activeSource.getAttribute('data-src')
+    },
+    isLazy: () => !activeSource.src
+  }
+}
+
+export type SourcesController = ReturnType<typeof sourcesController>
