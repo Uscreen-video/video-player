eaab63ba14c036af08c2c515c16693b1eafea531
Bitaru <i@bitaru.com>
2024-03-20 17:18:40 +0100

    chore(): create release flow

---
+++ release.config.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    "main",
+    {
+      name: "rc",
+      prerelease: true
+    }
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+}
