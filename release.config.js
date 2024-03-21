6e69bc98d42e0f66f29ff5897d65269b5cedaff8
Bitaru <i@bitaru.com>
2024-03-21 12:57:14 +0100

    chore: publish to npm

--- release.config.js
+++ release.config.js
@@ -12,7 +12,7 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    // "@semantic-release/npm",
+    "@semantic-release/npm",
     [
       "@semantic-release/github",
       {
