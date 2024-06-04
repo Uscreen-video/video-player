79d134960b6ae33c5ef767ea41de2e0cd9241179
timaramazanov <tima96ramazanov@gmail.com>
2024-06-04 15:05:19 +0100

    fix(): remove debug logs

--- src/components/video-container/Video-container.component.ts
+++ src/components/video-container/Video-container.component.ts
@@ -104,7 +104,6 @@ export class VideoContainer extends LitElement {
 
   @listen(Types.Command.seek, { canPlay: true })
   seek({ time }: { time: number }) {
-    console.log('GOING TO SEEK!!!')
     const [video] = this.videos;
     video.currentTime = time;
     if (video.paused && !this.castActivated) this.play();
@@ -343,7 +342,6 @@ export class VideoContainer extends LitElement {
         });
         break;
       case "loadeddata":
-        console.log('LOADED DATA!')
         dispatch(this, Types.Action.updateDuration, {
           initialized: true,
           duration: getVideoDuration(video),
@@ -374,7 +372,6 @@ export class VideoContainer extends LitElement {
         dispatch(this, Types.Action.togglePip, { pipActivated: false });
         break;
       case "loadedmetadata":
-        console.log('LOADED META DATA!')
         dispatch(this, Types.Action.canPlay);
         break;
       case "error":
